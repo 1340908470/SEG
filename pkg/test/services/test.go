121a3c1df953881b6d5dac7473c8ee13df25bf37
chenweihao.123 <chenweihao.123@bytedance.com>
2022-06-11 20:37:09 +0800

    get user in TestService.Test

--- pkg/test/services/test.go
+++ pkg/test/services/test.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"fmt"
+	"github.com/lantu-dev/puki/pkg/auth"
+	"github.com/lantu-dev/puki/pkg/auth/models"
 	"github.com/lantu-dev/puki/pkg/base/rpc"
 	"gorm.io/gorm"
 )
@@ -23,6 +25,21 @@ type TestRes struct {
 }
 
 func (s *TestService) Test(ctx *rpc.Context, req *TestReq, res *TestRes) (err error) {
+	//1. 获取User信息
+	var user *models.User
+
+	//获取创建者信息
+	var tokenUser auth.TokenUser
+	tokenUser, err = auth.ExtractTokenUser(ctx)
+	if err != nil {
+		return err
+	}
+
+	tx := s.db.Begin()
+	user = models.FindUserById(tx, tokenUser.ID)
+
+	print(user)
+
 	res.Message = fmt.Sprintf("hello, %v", req.Name)
 	return nil
 }
